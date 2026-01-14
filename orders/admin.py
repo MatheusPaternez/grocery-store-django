0e74c83c49a66eab0460ffe0dff845c33650b237
Matheus Paternez <matheusgfpaternez2006@gmail.com>
2026-01-14 15:40:42 -0800

    admin order done

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'status', 'created_at', 'get_total_cost']
+    list_filter = ['status', 'created_at']
+    inlines = [OrderItemInline]
+
+admin.site.register(OrderItem)
