9165636afff03e7c3104cd86acc4c774e66c4cd1
Matheus Paternez <matheusgfpaternez2006@gmail.com>
2026-01-14 20:51:19 -0800

    Patch home page design

--- core/views.py
+++ core/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from products.models import Product
 
 def home(request):
-    return render(request, 'home.html')
+    featured_products = Product.objects.all()[:4]
+    
+    context = {
+        'featured_products': featured_products
+    }
+    return render(request, 'home.html', context)
