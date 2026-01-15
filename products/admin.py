aa2697552fca24ad33c4b9d7784a512c6c8ef68a
Matheus Paternez <matheusgfpaternez2006@gmail.com>
2026-01-15 11:37:04 -0800

    Add admin view for products

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+admin.site.register(Category)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+    
+    list_filter = ['category']
+    
+    search_fields = ['name', 'description']
